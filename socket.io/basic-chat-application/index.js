489d6991ee3fc81cb037e13a5c60817caa451974
Zobeir Hamid <zobeirhamid@berkeley.edu>
2021-05-21 12:00:29 +0200

    Finishes first socket.io guide

---
+++ socket.io/basic-chat-application/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.broadcast.emit("user connected");
+
+  socket.on("chat message", (msg) => {
+    socket.broadcast.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
