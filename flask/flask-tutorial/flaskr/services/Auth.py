7e2532b61726760d854fbe33454b972fd3e7348e
Zobeir Hamid <zobeirhamid@berkeley.edu>
2021-09-09 19:41:07 -0700

    Finishes first Flask project

---
+++ flask/flask-tutorial/flaskr/services/Auth.py
@@ -0,0 +1,13 @@
+import functools
+from flask import (
+    redirect, g, url_for
+)
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
